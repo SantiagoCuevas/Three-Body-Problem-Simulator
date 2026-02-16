f76b1f75fe20f8705c4207af90cdd4c175a0297a
Santiago Cuevas <santiagocuevas77@gmail.com>
2026-02-15 19:53:44 -0700

    Add sphere

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
           <ambientLight intensity={0.1} />
           <directionalLight color="red" position={[0, 0, 5]} />
           <mesh>
-            <boxGeometry args={[2, 2, 2]} />
+            <sphereGeometry />
             <meshStandardMaterial />
           </mesh>
         </Canvas>
