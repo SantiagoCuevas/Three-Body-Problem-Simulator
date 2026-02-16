9070cd05a267a009d20b9ae2eb287404137ba793
Santiago Cuevas <santiagocuevas77@gmail.com>
2026-02-15 19:49:58 -0700

    Add camera rotation

--- src/components/CameraController.tsx
+++ src/components/CameraController.tsx
@@ -1,35 +1,53 @@
 import { useThree } from "@react-three/fiber";
 import { useEffect } from "react";
 
+enum MoveType {
+  POS = "position",
+  ROT = "rotation",
+}
+
+const moveMap: any = {
+  w: { axis: "z", by: -1, type: MoveType.POS },
+  s: { axis: "z", by: 1, type: MoveType.POS },
+  a: { axis: "x", by: -1, type: MoveType.POS },
+  d: { axis: "x", by: 1, type: MoveType.POS },
+  " ": { axis: "y", by: 1, type: MoveType.POS },
+  Shift: { axis: "y", by: -1, type: MoveType.POS },
+  ArrowUp: { axis: "x", by: 0.1, type: MoveType.ROT },
+  ArrowDown: { axis: "x", by: -0.1, type: MoveType.ROT },
+  ArrowLeft: { axis: "y", by: 0.1, type: MoveType.ROT },
+  ArrowRight: { axis: "y", by: -0.1, type: MoveType.ROT },
+} as const;
+
 export const CameraController = () => {
   const camera = useThree((state) => state.camera);
 
   useEffect(() => {
     window.addEventListener("keydown", (e) => {
-      const newPosition = {
-        x: camera.position.x,
-        y: camera.position.y,
-        z: camera.position.z,
+      console.log(e.key);
+      const newTransform = {
+        position: {
+          x: camera.position.x,
+          y: camera.position.y,
+          z: camera.position.z,
+        },
+        rotation: {
+          x: camera.rotation.x,
+          y: camera.rotation.y,
+          z: camera.rotation.z,
+        },
       };
 
-      switch (e.key) {
-        case "w": {
-          newPosition.z -= 1;
-          break;
-        }
-        case "s": {
-          newPosition.z += 1;
-          break;
-        }
-        case "a": {
-          newPosition.x -= 1;
-          break;
-        }
-        case "d": {
-          newPosition.x += 1;
-        }
-      }
-      camera.position.set(newPosition.x, newPosition.y, newPosition.z);
+      const move: any = moveMap[e.key];
+
+      if (!move) return;
+      e.preventDefault();
+      newTransform[move.type][move.axis] += move.by;
+      camera[move.type].set(
+        newTransform[move.type].x,
+        newTransform[move.type].y,
+        newTransform[move.type].z,
+      );
     });
   }, [camera]);
 
